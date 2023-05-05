7b3497f21f035bb42d682dd429e269a5661f1be9
Eva Nedeliaková <evanedeliakova@gmail.com>
2023-05-05 23:45:49 +0200

    static file settings for cluster + local url

--- pipeline_administration_django/settings.py
+++ pipeline_administration_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+import kfp
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,9 +134,24 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 # STATICFILES_DIRS = [U_BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'static'
+
+# Static files (CSS, JavaScript, Images)                   
+# https://docs.djangoproject.com/en/4.2/howto/static-files/           
+#STATIC_URL = 'static/'                                                                                
+# STATICFILES_DIRS = [U_BASE_DIR / "static"]                                                           
+STATIC_ROOT = BASE_DIR / 'static'                                                                      
+                                                                                                       
+                                                                                                       
+credentials = kfp.auth.ServiceAccountTokenVolumeCredentials(path=None)                                 
+client = kfp.Client(host="http://ml-pipeline.kubeflow.svc.cluster.local:8888", credentials=credentials)
+namespace = client.get_user_namespace()
+
+STATIC_URL = '/run-requests/' + namespace + '/static/'
+STATIC_URL_LOCAL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
