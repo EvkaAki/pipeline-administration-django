7b29eb280a8a8a8fbaa0de300fc8cbd200dc909e
AkiEvka <evanedeliakova@gmail.com>
2023-05-08 17:20:05 +0200

    style researcher view

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from .views import researcher_view
 
 urlpatterns = [
     path("", researcher_view, name="researcher"),
+    path("admin", researcher_view, name="admin"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import kfp
 import os
 
 
 def researcher_view(request):
-    pipeline_url = os.environ.get("PIPELINE_URL")
     credentials = kfp.auth.ServiceAccountTokenVolumeCredentials(path=None)
-    client = kfp.Client(host=pipeline_url, credentials=credentials)
-    # namespace = client.get_user_namespace()
+    client = kfp.Client(host=os.environ.get("PIPELINE_URL"), credentials=credentials)
+    namespace = client.get_user_namespace()
+    if namespace == 'admin':
+        return redirect("admin", foo="bar")
     pipelines = client.list_pipelines()
     dev_mode = os.environ.get("DEV_MODE")
     print('here')
 
     return render(request, 'researcher.html', {'pipelines': pipelines.pipelines, 'bla': dev_mode})
+
+
+def admin_view(request):
+    return render(request, 'hello.html', {})
